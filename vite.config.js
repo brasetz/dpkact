ac87ad877137fcbdbe098d3d06f0cd1637ea7db0
DEEPAK SINGH <connectbrasetz@gmail.com>
2025-01-14 10:11:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-  },
   plugins: [react()],
-})
-
+  build: {
+    target: 'esnext', // Use modern JavaScript for faster builds
+    minify: 'esbuild', // Fast minification
+    sourcemap: false, // Disable source maps in production
+    chunkSizeWarningLimit: 500, // Adjust chunk size warning limit if needed
+  },
+});
